08350d8557058eeffd5d2110a6d2430227624bfd
Henrique Moraes <ahmdesouza@gmail.com>
2022-06-27 17:56:13 -0300

    Initial commit
    commits para arrumar o arquivo

---
+++ index.php
@@ -0,0 +1,17 @@
+include("config/config.php");?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendario SNGL</title>
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/css/style.css'; ?>">
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/js/fullcalendar/main.min.css'; ?>">
+</head>
+<body>
+    <div class="calendar"></div>
+    <script src="<?php echo DIRPAGE.'lib/js/fullcalendar/main.min.js'; ?>"></script>
+    <script src="<?php echo DIRPAGE.'lib/js/javascript.js'; ?>"></script>
+</body>
+</html>
---
+++ lib/composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wef/calendar',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5880106a1e18bd88ddfbd9f62bebf69a5a33d298',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wef/calendar' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5880106a1e18bd88ddfbd9f62bebf69a5a33d298',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
